1fef1c03ed1f409ae9afc2a553fc16f44f144dba
A712\dam1 <mcheickmoussa@gmail.com>
2025-12-03 21:11:59 +0100

    bat1,2,3

--- Modular/src/main/java/org/example/Batería_3Cadena.java
+++ Modular/src/main/java/org/example/Batería_3Cadena.java
@@ -1,6 +1,5 @@
 package org.example;
 
-import java.beans.Introspector;
 import java.util.Scanner;
 
 public class Batería_3Cadena {
@@ -11,5 +10,61 @@ public class Batería_3Cadena {
 
         System.out.println("Introduce una cadena de palabras");
 
+        String cadena = teclado.nextLine();
+        //son cadenas diferentes
+        cadena = mayus(cadena);
+
+        System.out.println(cadena);
+
+        int total_vocales=contarVocales(cadena);
+        System.out.println("Vocales totales " + total_vocales);
+
+
+    }
+
+    public static String mayus(String cadena){
+
+      //  cadena = cadena.toUpperCase(); return cadena;
+        return cadena.toUpperCase();
+
+    }
+
+    public static int contarVocales(String cadena){
+
+        int a =0;
+        int e =0;
+        int i =0;
+        int o =0;
+        int u =0;
+        for (int j = 0; j <cadena.length(); j++) {
+
+            switch (cadena.charAt(j)){
+
+                case 'A':
+                    a++;
+                    break;
+                case 'E':
+                    e++;
+                    break;
+                case 'I':
+                    i++;
+                    break;
+                case 'O':
+                    o++;
+                    break;
+                case 'U':
+                    u++;
+                    break;
+
+            }
+
+        }
+        System.out.println("A: " + a);
+        System.out.println("e: " + e);
+        System.out.println("i: " + i);
+        System.out.println("o: " + o);
+        System.out.println("u: " + u);
+
+        return a+e+i+o+u;
     }
 }
